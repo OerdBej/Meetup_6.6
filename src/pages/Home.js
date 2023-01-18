95cbf66a1eb9045ea89f9a29e15489b5b06fd3fd
Oerd Bej <oerdbej@gmail.com>
2023-01-18 21:51:01 +0100

    adding programmatically the button to change the page route

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/products");
+  }
+
   return (
     <>
       <h3>This is the Home Page</h3>
       <p>
         Go to <Link to='/products'>Products</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate </button>
       </p>
     </>
   );
